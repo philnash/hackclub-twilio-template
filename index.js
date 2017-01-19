5255da677b6bbaae7909360b48fbd4ad5c00ec86
Phil Nash <philnash@gmail.com>
2017-01-18 16:09:33 -0800

    :sparkles: Pre-require the dependencies

---
+++ index.js
@@ -0,0 +1,6 @@
+// Require the dependencies
+var twilio = require('twilio');
+var express = require('express');
+var bodyParser = require('body-parser');
+var fetch = require('node-fetch');
+var escape = require('querystring').escape;
